93c1fc267cb3061841a07a1cbdc5b09e226f0cef
CABARDOJAELLANDREI <jaellandreic@gmail.com>
2026-07-11 11:37:22 +0800

    MealRecipe.cs

--- IT_ELECTIVE_2_PRELIM_EXAM/Models/MealRecipe.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM/Models/MealRecipe.cs
@@ -28,6 +28,8 @@ public class MealRecipe : RecipeBase //, IRecipeSearchable  <-- EXERCISE 9: Unco
     public MealRecipe(string title, int prepTime, string difficulty)
         : base(title, prepTime, difficulty)
     {
+        Category = title;
+        Area = Area;
     }
 
     // EXERCISE 7: Create a constructor that also accepts category and area
@@ -36,10 +38,18 @@ public class MealRecipe : RecipeBase //, IRecipeSearchable  <-- EXERCISE 9: Unco
     public override string GetRecipeInfo()
     {
         // EXERCISE 7: Override to include Category and Area in the output
-        return base.GetRecipeInfo();
+        return $"{base.GetRecipeInfo()} | Category: {Category} | Area: {Area}";
     }
 
     // EXERCISE 9: Implement IRecipeSearchable interface methods here
     // public string SearchCriteria => ???
     // public bool MatchesSearch(string searchTerm) => ???
+
+    public string SearchCriteria => Title;
+
+    public bool MatchesSearch(String searchTerm)
+    {
+        if(string .IsNullOrEmpty(searchTerm)) return false;
+        return Title.Contains(searchTerm, StringComparison.OrdinalIgnoreCase);
+    }
 }
