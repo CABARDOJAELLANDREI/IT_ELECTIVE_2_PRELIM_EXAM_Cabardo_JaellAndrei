fb2809b7088648201029aede263ece1a888aee7c
CABARDOJAELLANDREI <jaellandreic@gmail.com>
2026-07-11 12:27:12 +0800

    SearchMealByName.cs

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/SearchMealByName.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/SearchMealByName.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 2: GET Search by Name
@@ -15,11 +20,31 @@ public static class SearchMealByName
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
-        // TODO: Send GET request to https://themealdb.com/api/json/v1/1/search.php?s=Arrabiata
-        // TODO: Assert status code is 200 OK
-        // TODO: Parse the response JSON
-        // TODO: Assert that the "meals" array is not null and has at least 1 item
+        string url = "https://themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
 
-        throw new NotImplementedException();
+        // 1. Use the HttpClient to search for meals with name "Arrabiata"
+        HttpResponseMessage response = await client.GetAsync(url);
+
+        // 2. Assert status code is 200 OK
+        if (response.StatusCode != System.Net.HttpStatusCode.OK)
+        {
+            throw new Exception($"Assertion failed: Status code was {response.StatusCode}, expected 200 OK.");
+        }
+
+        // 3. Parse the response JSON
+        string responseString = await response.Content.ReadAsStringAsync();
+        using JsonDocument doc = JsonDocument.Parse(responseString);
+        JsonElement root = doc.RootElement;
+
+        // Assert that the "meals" array is not null/missing and has at least 1 item
+        if (!root.TryGetProperty("meals", out JsonElement mealsArray) || mealsArray.ValueKind != JsonValueKind.Array)
+        {
+            throw new Exception("Assertion failed: Response JSON does not contain a valid 'meals' array.");
+        }
+
+        if (mealsArray.GetArrayLength() < 1)
+        {
+            throw new Exception("Assertion failed: The 'meals' array does not have at least 1 item.");
+        }
     }
 }
