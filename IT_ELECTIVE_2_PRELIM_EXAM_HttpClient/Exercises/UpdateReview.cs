015de371e9d7f444ad1d66a1c0ae84c22c7a3ead
CABARDOJAELLANDREI <jaellandreic@gmail.com>
2026-07-11 12:29:27 +0800

    UpdateReview.cs

--- IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/UpdateReview.cs
+++ IT_ELECTIVE_2_PRELIM_EXAM_HttpClient/Exercises/UpdateReview.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
 namespace IT_ELECTIVE_2_PRELIM_EXAM_HttpClient.Exercises;
 
 // EXERCISE 7: PUT Update Review
@@ -16,13 +22,38 @@ public static class UpdateReview
 {
     public static async Task Run(System.Net.Http.HttpClient client)
     {
-        // TODO: Create JSON string with id, title, body, and userId
-        // TODO: Create StringContent with the JSON and Content-Type "application/json"
-        // TODO: Send PUT request to https://jsonplaceholder.typicode.com/posts/1
-        // TODO: Assert status code is 200 OK
-        // TODO: Parse the response JSON
-        // TODO: Assert the title is "Updated Review"
+        string url = "https://jsonplaceholder.typicode.com/posts/1";
 
-        throw new NotImplementedException();
+        // 1. Create a JSON body string
+        string json = "{\"id\": 1, \"title\": \"Updated Review\", \"body\": \"Even better than before!\", \"userId\": 1}";
+
+        // 2. Wrap it in StringContent with media type "application/json"
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        // 3. Send a PUT request to update post with ID 1
+        HttpResponseMessage response = await client.PutAsync(url, content);
+
+        // 4. Assert status code is 200 OK
+        if (response.StatusCode != System.Net.HttpStatusCode.OK)
+        {
+            throw new Exception($"Assertion failed: Status code was {response.StatusCode}, expected 200 OK.");
+        }
+
+        // 5. Parse the response JSON
+        string responseString = await response.Content.ReadAsStringAsync();
+        using JsonDocument doc = JsonDocument.Parse(responseString);
+        JsonElement root = doc.RootElement;
+
+        // Assert the title is "Updated Review"
+        if (!root.TryGetProperty("title", out JsonElement titleProp))
+        {
+            throw new Exception("Assertion failed: Response JSON does not contain a 'title' field.");
+        }
+
+        string titleValue = titleProp.GetString();
+        if (!string.Equals(titleValue, "Updated Review", StringComparison.OrdinalIgnoreCase))
+        {
+            throw new Exception($"Assertion failed: Expected title 'Updated Review', but got '{titleValue}'.");
+        }
     }
 }
